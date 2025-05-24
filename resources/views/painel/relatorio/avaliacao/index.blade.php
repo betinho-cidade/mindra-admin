e63579ab6897b00a18d834a932ce09f8b253ea04
Betinho Cidade <betinho.cidade@gmail.com>
2025-05-24 10:10:00 -0300

    first commit

--- resources/views/painel/relatorio/avaliacao/index.blade.php
+++ resources/views/painel/relatorio/avaliacao/index.blade.php
@@ -35,8 +35,17 @@
                 <h2 class="text-xl font-semibold mb-4 text-gray-700">Campanhas Ativas/Em Andamento</h2>
                 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     @foreach($campanha_funcionarios->whereNull('data_realizado') as $campanha_funcionario)
-                        <div class="bg-green-50 rounded-lg p-4 shadow hover:shadow-md transition-shadow h-40 overflow-y-auto">
-                            <h3 class="text-sm font-semibold text-green-600"><a href="javascript:;" onclick="formulario('{{ $campanha_funcionario->id }}')">{{ $campanha_funcionario->campanha_empresa->campanha->formulario->titulo }}</a></h3>
+                        <div class="bg-{{ ($campanha_funcionario->campanha_empresa->campanha->data_fim < \Carbon\Carbon::now()) ? 'gray-100' : 'green-50' }} rounded-lg p-4 shadow hover:shadow-md transition-shadow h-40 overflow-y-auto {{ ($campanha_funcionario->campanha_empresa->campanha->data_fim < \Carbon\Carbon::now()) ? 'opacity-70' : '' }}">
+
+                            @if($campanha_funcionario->campanha_empresa->campanha->data_fim < \Carbon\Carbon::now())
+                            <div class="flex justify-between items-start">
+                                <h3 class="text-sm font-semibold text-green-600">{{ $campanha_funcionario->campanha_empresa->campanha->formulario->titulo }}</h3>
+                                <span class="bg-red-300 text-red-700 text-xs font-medium px-2 py-1 rounded">Expirado</span>
+                            </div>
+                            @else
+                                <h3 class="text-sm font-semibold text-green-600"><a href="javascript:;" onclick="formulario('{{ $campanha_funcionario->id }}')">{{ $campanha_funcionario->campanha_empresa->campanha->formulario->titulo }}</a></h3>
+                            @endif
+
                             <p class="text-xs text-gray-600"><strong class="font-extrabold text-sm">Empresa:</strong> {{ $campanha_funcionario->campanha_empresa->empresa->nome }}</p>
                             <p class="text-xs text-gray-600"><strong class="font-extrabold text-sm">Campanha:</strong> {{ $campanha_funcionario->campanha_empresa->campanha->titulo }}</p>
                             <p class="text-xs text-gray-500"><strong class="font-extrabold text-sm">Período:</strong> {{ $campanha_funcionario->campanha_empresa->campanha->periodo }}</p>
